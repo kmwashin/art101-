dc1b23aa96d1ae1c19611d4bfe5dcfa22bd410ca
kmwashin <64337304+kmwashin@users.noreply.github.com>
2022-10-17 17:33:52 -0700

    lab4
    lab4 and maybe? lab3 was having a hard time pushing to github.

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+/*
+* Summary.
+*
+* Description.
+*
+* @file This files defines the MyClass class.
+* @author @Keiko @Kenice
+* @since 10.12.22
+*/
+// Take user input
+// Accept user's input only if it's a number
+  //'if' statement to check if user input is a number
+  //if not a number prompt user again
+// Print new line
+// Multiply user's number by 2
+// Display the product
