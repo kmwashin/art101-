39cbdfec58e7d3cdd3753cf89d7ff79855f1acc5
kmwashin <64337304+kmwashin@users.noreply.github.com>
2022-10-18 00:59:39 -0700

    lab5
    we finished lab5 yay!

---
+++ lab5/js/lab.js
@@ -0,0 +1,32 @@
+/*
+* Summary.
+*
+* Description.
+*
+* @file This files defines the MyClass class.
+* @author @Keiko @Kenice
+* @since 10.12.22
+*/
+// Take user input
+// Accept user's input only if it's a number
+  //'if' statement to check if user input is a number
+  //if not a number prompt user again
+// Print new line
+// Multiply user's number by 2
+// Display the product
+
+//define variables
+make = "toyota";
+model = "corolla";
+color = "mold green with a pale yellow underbelly and amber speckles";
+year ="600";
+
+//calculate
+age = 2022-year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " BBY<br>");
